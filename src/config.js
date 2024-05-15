418e65dd01e96625c513eeabb11320001d756a99
francisco9505 <franciscocastillo9505@gmail.com>
2024-05-15 14:47:14 +0200

    change api key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=162375357f0eaf1dd8caba47a001b2e9";
+const API_KEY_PARAM = "?api_key=edf3ada58636b77ba94efac9b0cc220c";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
 
